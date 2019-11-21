e0df5168edab09fb3e12c77d10df5ae648113dcf
FarisLucky <luckyfaris44@gmail.com>
2019-11-21 15:32:45 +0700

    front end layout
    Sudah ada front end untuk menu kategori

---
+++ front/js/custom.js
@@ -0,0 +1,16 @@
+$(function () {
+    console.log(scroll);
+    $('.cs-copyright').click(function (e) { 
+        e.preventDefault();
+        window.scrollTo({top: 0,behavior:'smooth'});
+    });
+    var scroll,offset_y = 60;
+    document.onscroll = ()=>{
+        scroll = $(window).scrollTop();
+        if (scroll >= offset_y) {
+            $('.container-desktop').addClass('tsy-30');
+        } else{
+            $('.container-desktop').removeClass('tsy-30');
+        }
+    }
+});
