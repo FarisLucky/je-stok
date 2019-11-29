8e1e36277529787bffa331b1abdf47963a9fef4c
Ridwanizer <ridwanhanantoaji@gmail.com>
2019-11-29 14:42:58 +0700

    update
    menu kategori sub menu

---
+++ application/controllers/admin/Sub_Menu.php
@@ -0,0 +1,140 @@
+<?php
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Sub_Menu extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('ModelApp');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Sub_Menu';
+        $select = '*';
+        $tbl = 'sub_menu';
+        $tbl2 = 'menu_grup';
+        $data['submen'] = $this->ModelApp->getData($select, $tbl)->result_array();
+        $data['menu'] = $this->ModelApp->getData($select, $tbl2)->result_array();
+        $this->load->view('backend/sub_menu/sub_menu', $data);
+    }
+
+    public function tambah()
+    {
+        $data['title'] = 'Tambah Menu';
+        $select = '*';  
+        $tbl2 = 'menu_grup';
+        $data['menu'] = $this->ModelApp->getData($select, $tbl2)->result_array();
+        $this->load->view('backend/sub_menu/sub_menu_tambah', $data);
+    }
+    public function ubah($id_sub_menu)
+    {
+        $data['title'] = 'Ubah Menu';
+        $select = '*';
+        $tbl = 'sub_menu';
+        $tbl2 = 'menu_grup';
+        $where = ['id_sub_menu' => $id_sub_menu];
+        $data['submen'] = $this->ModelApp->getData($select, $tbl, $where)->row_array();
+        $data['menu'] = $this->ModelApp->getData($select, $tbl2)->result_array();
+        $this->load->view('backend/sub_menu/sub_menu_ubah', $data);
+    }
+    public function coreTambah()
+    {
+        $this->form_validation->set_rules('i_nama_sub_menu', 'Nama Sub Menu', 'trim|required|min_length[3]|max_length[50]');
+        $this->form_validation->set_rules('i_menu', 'Menu', 'trim|required');
+
+        if ($this->form_validation->run() == FALSE) {
+
+            $this->tambah();
+        } else {
+
+            $input_nama = $this->input->post('i_nama_sub_menu', true);
+            $input_menu = $this->input->post('i_menu', true);
+            $data_input = ['id_menu' => $input_menu];
+            $data_input += ['nama' => $input_nama];
+            $tbl = 'sub_menu';
+            $tambah_menu = $this->ModelApp->insertData($data_input, $tbl);
+
+            if ($tambah_menu) {
+
+                $this->session->set_flashdata('success', 'Data Berhasil ditambahkan');
+                redirect('admin/sub_menu/tambah');
+            } else {
+
+                $this->session->set_flashdata('failed', 'Data gagal ditambahkan');
+                redirect('admin/sub_menu/tambah');
+            }
+        }
+    }
+    public function coreUbah()
+    {
+        $id_sub_menu = $this->input->post('input_hidden', true);
+        $select = '*';
+        $tbl = 'sub_menu';
+        $where = ['id_sub_menu' => $id_sub_menu];
+        $get_menu = $this->ModelApp->getData($select, $tbl, $where);
+        if ($get_menu->num_rows() > 0) {
+
+            $this->form_validation->set_rules('i_nama_sub_menu', 'Nama Sub Menu', 'trim|required|min_length[3]|max_length[50]');
+            $this->form_validation->set_rules('i_menu', 'Menu', 'trim|required');
+            
+            if ($this->form_validation->run() == FALSE) {
+
+                $this->ubah($id_sub_menu);
+            } else {
+
+                $input_nama = $this->input->post('i_nama_sub_menu', true);
+                $input_menu = $this->input->post('i_menu', true);
+                $data_input = ['id_menu' => $input_menu];
+                $data_input += ['nama' => $input_nama];
+                $tambah_sub_menu = $this->ModelApp->updateData($data_input, $tbl, $where);
+
+                if ($tambah_sub_menu) {
+
+                    $this->session->set_flashdata('success', 'Data Berhasil diubah');
+                    redirect('admin/sub_menu/ubah/' . $id_sub_menu);
+                } else {
+
+                    $this->session->set_flashdata('failed', 'Data gagal diubah');
+                    redirect('admin/sub_menu/ubah' . $id_sub_menu);
+                }
+            }
+        } else {
+
+            $data['heading'] = 'Kesalahan 504';
+            $data['message'] = 'Data tidak ditemukan';
+            $this->load->view('errors/error_user', $data);
+        }
+    }
+
+    public function hapus($id_sub_menu)
+    {
+        $select = '*';
+        $tbl = 'sub_menu';
+        $where = ['id_sub_menu' => $id_sub_menu];
+        $get_menu = $this->ModelApp->getData($select, $tbl, $where);
+        if ($get_menu->num_rows() > 0) {
+
+            $hapus_sub_menu = $this->ModelApp->deleteData($where, $tbl);
+            if ($hapus_sub_menu) {
+
+                $this->session->set_flashdata('success', 'Data Berhasil dihapus');
+                redirect('admin/sub_menu');
+            } else {
+
+                $this->session->set_flashdata('failed', 'Data gagal dihapus');
+                redirect('admin/sub_menu');
+            }
+        } else {
+            $data['heading'] = 'Kesalahan 504';
+            $data['message'] = 'Data tidak ditemukan';
+            $this->load->view('errors/error_user', $data);
+        }
+    }
+}
+
+/* End of file Dashboard.php */
--- application/views/backend/kategori/kategori.php
+++ application/views/backend/kategori/kategori.php
@@ -4,57 +4,49 @@
 <div class="container-fluid">
   <!-- Header Title -->
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Customer</h1>
+    <h1 class="h3 mb-0 text-gray-800">Kategori Group</h1>
   </div>
   <!-- !Header Title -->
   <!-- Button Insert -->
-  <!-- <div class="row my-4">
+  <div class="row my-4">
     <div class="col-sm-12">
-      <a href="base_url('admin/customer/tambah')" class="btn btn-sm btn-primary float-right"><span class="fa fa-plus"></span> Tambah</a>
+      <a href="<?= base_url('admin/kategori/tambah') ?>" class="btn btn-sm btn-primary float-right"><span class="fa fa-plus"></span> Tambah</a>
     </div>
-  </div> -->
+  </div>
   <!-- Button Insert -->
   <!-- Table menu -->
   <div class="row">
     <div class="col-lg-12">
       <div class="card shadow mb-4">
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          <h6 class="m-0 font-weight-bold text-primary">Table Daftar Customer</h6>
+          <h6 class="m-0 font-weight-bold text-primary">Table Kategori</h6>
         </div>
         <div class="card-body">
           <div class="responsive">
             <table class="table table-hover">
               <thead class="table-primary">
                 <th>No</th>
-                <th>Nama Lengkap</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Telepon</th>
+                <th>Kategori</th>
+        
                 <th>Aksi</th>
               </thead>
               <tbody>
-                <?php $no = 1 + $row;
-                foreach ($user as $key => $value) { ?>
+                <?php $no = 1;
+                foreach ($katgor as $key => $value) { ?>
+                
                   <tr>
                     <td><?= $no ?></td>
-                    <td><?= $value['nama_lengkap'] ?></td>
-                    <td><?= $value['username']  ?></td>
-                    <td><?= $value['email'] ?></td>
-                    <td><?= $value['telp'] ?></td>
+                    <td><?= $value['nama'] ?></td>
+                  
                     <td>
-                      
-                      <button class="btn btn-outline-danger mx-2" onclick="deleteData('<?= base_url('admin/customer/hapus/' . $value['id_user']) ?>','Hapus')"><span class="fa fa-trash"></span></button>
+                      <a href="<?= base_url('admin/kategori/ubah/' . $value['id_sub_menu']) ?>" class="btn btn-outline-info"><span class="fa fa-edit"></span></a>
+                      <button class="btn btn-outline-danger mx-2" onclick="deleteData('<?= base_url('admin/kategori/hapus/' . $value['id_kategori']) ?>','Hapus')"><span class="fa fa-trash"></span></button>
                     </td>
                   </tr>
                 <?php $no++;
                 } ?>
               </tbody>
             </table>
-          </div>
-          <div class="row justify-content-center">
-            <nav class="mt-3">
-              <?php echo $this->pagination->create_links(); ?>
-            </nav>
           </div>
         </div>
       </div>
