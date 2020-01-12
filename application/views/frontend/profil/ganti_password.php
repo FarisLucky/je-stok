f3482fec82f42f0af1e0d9b5261d54d931c5b20a
fanifadil <44403837+fanifadil@users.noreply.github.com>
2020-01-12 12:37:58 +0700

    asd

--- application/views/frontend/profil/ganti_password.php
+++ application/views/frontend/profil/ganti_password.php
@@ -9,25 +9,32 @@
           <h4 class="profil">Ganti Kata Sandi</h4>
         </div>
         <div class="right-body">
-          <div class="row">
-            <div class="col-md-6">
-              <div class="form-group">
-                <label>Password Lama</label>
-                <input type="text" name="i_password_lama" class="form-control">
-              </div>
-              <div class="form-group">
-                <label>Password Baru</label>
-                <input type="text" name="i_password_baru" class="form-control">
-              </div>
-              <div class="form-group">
-                <label>Konfirmasi Password Baru</label>
-                <input type="text" name="i_konfirmasi_password" class="form-control">
+
+          <form method="POST" href="<?= base_url('Profil/gantipassword'); ?>">
+            <div class="row">
+              <div class="col-md-6">
+                <?= $this->session->flashdata('message'); ?>
+                <div class="form-group">
+                  <label>Password Lama</label>
+                  <input type="password" name="password_lama" class="form-control">
+                  <?= form_error('password_lama', '<small class = "text-danger pl-3">', '</small>'); ?>
+                </div>
+                <div class="form-group">
+                  <label>Password Baru</label>
+                  <input type="password" name="password_baru1" class="form-control">
+                  <?= form_error('password_baru1', '<small class = "text-danger pl-3">', '</small>'); ?>
+                </div>
+                <div class="form-group">
+                  <label>Konfirmasi Password Baru</label>
+                  <input type="password" name="password_baru2" class="form-control">
+                  <?= form_error('password_baru2', '<small class = "text-danger pl-3">', '</small>'); ?>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="row-btn">
-            <button type="submit" class="btn btn-primary btn-simpan">Simpan</button>
-          </div>
+            <div class="row-btn">
+              <button type="submit" class="btn btn-primary btn-simpan">Simpan</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- application/views/frontend/profil/sidebar_component.php
+++ application/views/frontend/profil/sidebar_component.php
@@ -1,22 +1,34 @@
 <div class="left-sidebar">
   <h5>Menu</h5>
   <ul class="sidebar-custom">
-    <li class="sidebar-item active">
-      <a href="#" class="sidebar-link">Profile</a>
+    <li class="sidebar-item <?php if ($title == 'Profil') {
+                              echo 'active';
+                            } ?>">
+      <a href="<?= base_url('Profil'); ?>" class="sidebar-link">Profil</a>
     </li>
-    <li class="sidebar-item">
+    <li class="sidebar-item <?php if ($title == 'Ulasan') {
+                              echo 'active';
+                            } ?>">
       <a href="#" class="sidebar-link">Ulasan</a>
     </li>
-    <li class="sidebar-item">
-      <a href="#" class="sidebar-link">Riwayat Transaksi</a>
+    <li class="sidebar-item <?php if ($title == 'Riwayat Transaksi') {
+                              echo 'active';
+                            } ?>">
+      <a href="<?= base_url('Profil/riwayattransaksi'); ?>" class="sidebar-link">Riwayat Transaksi</a>
     </li>
-    <li class="sidebar-item">
+    <li class="sidebar-item <?php if ($title == 'Komplain') {
+                              echo 'active';
+                            } ?>">
       <a href="#" class="sidebar-link">Komplain</a>
     </li>
-    <li class="sidebar-item">
-      <a href="#" class="sidebar-link">Profile</a>
+    <li class="sidebar-item <?php if ($title == 'Ganti Password') {
+                              echo 'active';
+                            } ?>">
+      <a href="<?= base_url('Profil/gantipassword'); ?>" class="sidebar-link">Ganti Password</a>
     </li>
-    <li class="sidebar-item">
+    <li class="sidebar-item <?php if ($title == 'Pengaturan') {
+                              echo 'active';
+                            } ?>">
       <a href="#" class="sidebar-link">Pengaturan</a>
     </li>
   </ul>
